cfe55fceb3ec3bd3c429ca2926639e8b32ac271d
Kareem Ali <kareem.ali.rahman@gmail.com>
2020-10-12 11:55:00 +0200

    initial commit

---
+++ src/test/java/com/kareem/selenium/tests/CodilityIT.java
@@ -0,0 +1,105 @@
+package com.kareem.selenium.tests;
+
+import com.kareem.selenium.DriverBase;
+//import com.kareem.selenium.page_objects.GoogleHomePage;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import java.util.List;
+import org.testng.Assert;
+
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import java.util.concurrent.TimeUnit;
+//import org.junit.Assert.assert.*;
+
+
+public class CodilityIT extends DriverBase {
+
+    //private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
+        //return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+    //}
+
+   @Test
+   public void testLoginFormPresent() throws Exception{
+     WebDriver webDriver = getDriver();
+     List<WebElement> email = webDriver.findElements(By.id("email-input"));
+     List<WebElement> password= webDriver.findElements(By.id("password-input"));
+     List<WebElement> button = webDriver.findElements(By.id("login-button"));
+     Assert.assertTrue(!email.isEmpty());
+     Assert.assertTrue(!password.isEmpty());
+     Assert.assertTrue(!button.isEmpty());
+   }
+
+   public void enterEmailPassword(String emailInput, String passwordInput) throws Exception{
+     WebDriver webDriver = getDriver();
+     WebElement email = webDriver.findElement(By.id("email-input"));
+     WebElement password= webDriver.findElement(By.id("password-input"));
+     WebElement button = webDriver.findElement(By.id("login-button"));
+     email.sendKeys(emailInput);
+     password.sendKeys(passwordInput);
+     button.click();
+   }
+
+   @Test
+   public void testValidCredentialsWork() throws Exception{
+     WebDriver webDriver = getDriver();
+     //WebwebDriverWait
+     WebDriverWait wait = new WebDriverWait(webDriver, 3);
+     String validEmail = "login@codility.com";
+     String validPassword = "password";
+     enterEmailPassword(validEmail, validPassword);
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".message.success")));
+     WebElement msg = webDriver.findElement(By.cssSelector(".message.success"));
+     //Assert.assertEquals(msg.getText(), "Welcome to Codility");
+     Assert.assertTrue(msg.getText()=="Welcome to Codility");
+   }
+
+   @Test
+   public void testInvalidCredentialsDoesntWork() throws Exception{
+     WebDriver webDriver = getDriver();
+     //WebwebDriverWait
+     WebDriverWait wait = new WebDriverWait(webDriver, 3);
+     String invalidEmail = "unknown@codility.com";
+     String invalidPassword = "Password";
+     enterEmailPassword(invalidEmail, invalidPassword);
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".message.error")));
+     WebElement msg = webDriver.findElement(By.cssSelector(".message.error"));
+     Assert.assertEquals(msg.getText(), "You shall not pass! Arr!");
+   }
+
+   @Test
+   public void testInvalidEmailFormDoesntWork() throws Exception{
+     WebDriver webDriver = getDriver();
+     //WebwebDriverWait
+     WebDriverWait wait = new WebDriverWait(webDriver, 3);
+     String invalidEmail = "unknown@codility";
+     String invalidPassword = "Password";
+     enterEmailPassword(invalidEmail, invalidPassword);
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".validation.error")));
+     WebElement msg = webDriver.findElement(By.cssSelector(".validation.error"));
+     Assert.assertEquals(msg.getText(), "Enter a valid email");
+   }
+
+   @Test
+   public void testEmptyCredentialsDoesntWork() throws Exception{
+     WebDriver webDriver = getDriver();
+     //WebwebDriverWait
+     WebDriverWait wait = new WebDriverWait(webDriver, 3);
+     String emptyEmail = "";
+     String emptyPassword = "";
+     String validEmail = "login@codility.com";
+     String validPassword = "password";
+     enterEmailPassword(emptyEmail, validPassword);
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".validation.error")));
+     WebElement msg = webDriver.findElement(By.cssSelector(".validation.error"));
+     Assert.assertEquals(msg.getText(), "Email is required");
+     enterEmailPassword(validEmail, emptyPassword);
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".validation.error")));
+     msg = webDriver.findElement(By.cssSelector(".validation.error"));
+     Assert.assertEquals(msg.getText(), "Password is required");
+   }
+}
---
+++ src/test/java/com/kareem/selenium/tests/RandomUserIT.java
@@ -0,0 +1,66 @@
+package com.kareem.selenium.tests;
+
+import com.kareem.selenium.DriverBase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+public class RandomUserIT extends DriverBase {
+
+    private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
+        return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+    }
+
+    @Test
+    public void randomUserTwitterLink() throws Exception {
+        // Create a new WebDriver instance
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = getDriver();
+        
+
+        // And now use this to visit Google
+        driver.get("https://randomuser.me/");
+
+        WebElement img = driver.findElement(By.xpath("//div[contains(@class, 'card')]//img"));
+        //Assert.assertNotEquals(, 0, "image is not present");
+        //Assert.assertTrue(img.isDisplayed());
+
+        //JavascriptExecutor js = (JavascriptExecutor) driver;
+        //js.executeScript("arguments[0].hidden = true",img);
+        //List<WebElement> imgs1 = driver.findElements(By.xpath("//div[contains(@class, 'card')]//img"));
+        //Assert.assertTrue(imgs1.get(0).isDisplayed());
+
+        WebElement link = driver.findElement(By.className("twitter"));
+        String linkText = link.getAttribute("href");
+        Assert.assertEquals(linkText, "https://twitter.com/randomapi", "href link is not what is expected");
+        Assert.assertTrue(link.isDisplayed());
+        
+        
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        //GoogleHomePage googleHomePage = new GoogleHomePage();
+
+        // Check the title of the page
+        //System.out.println("Page title is: " + driver.getTitle());
+
+        //googleHomePage.enterSearchTerm("Cheese")
+                //.submitSearch();
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        //WebDriverWait wait = new WebDriverWait(driver, 10, 100);
+        //wait.until(pageTitleStartsWith("Cheese"));
+
+        //System.out.println("Page title is: " + driver.getTitle());
+    }
+
+}
